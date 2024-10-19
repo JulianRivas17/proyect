7e5b160984461772e02451a016f5e804c9799a11
Juli <rivasaaron154@gmail.com>
2024-10-19 15:04:13 -0300

    ajustes

--- backend/api/register/views.py
+++ backend/api/register/views.py
@@ -1,11 +1,12 @@
-# backend/api/views.py
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes  # Asegúrate de importar permission_classes
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
+from rest_framework.permissions import AllowAny
 
 @api_view(['POST'])
+@permission_classes([AllowAny])  
 def register_user(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
