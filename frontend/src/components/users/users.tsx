788fe7fab63ad9ebf8c4b12bc51f503f563ab6a9
Anahi Peralta <anahiperalta2727@gmail.com>
2024-10-30 00:53:40 -0300

    añadi breadcrums

--- frontend/src/components/users/users.tsx
+++ frontend/src/components/users/users.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { Layout, Card, Button, Table } from 'antd';
+import { Layout, Card, Button, Table, Breadcrumb} from 'antd';
 import { EditOutlined, DeleteOutlined, PlusOutlined, ExportOutlined } from '@ant-design/icons';
 import moment from 'moment';
 import './users.css';
+import { MenuOutlined } from '@ant-design/icons';
 
 const { Sider, Content } = Layout;
 
@@ -52,38 +53,53 @@ const Users: React.FC = () => {
     ];
 
     return (
-        <Layout style={{ minHeight: '100vh', overflow: 'hidden' }}>
-            <Sider width={250} className="sider">
-                <Card title="Filtros" bordered={false} className="filters-card">
-                    <Button type="primary" className="clear-filters-button">Limpiar filtros</Button>
-                </Card>
-            </Sider>
+        <div style={{ marginTop: '4rem'}}>
+            <div className="container-bread-crumb" style={{display: 'flex', alignItems: 'center', padding:'15px 0px'}}>
+                            <MenuOutlined style={{ fontSize: '14px', marginRight: '8px' }} />
+                            <Breadcrumb>
+                                <Breadcrumb.Item>Inicio</Breadcrumb.Item>
+                                <Breadcrumb.Item className="item-focus">Gestión de Empleados</Breadcrumb.Item>
+                            </Breadcrumb>
+            </div>
+            <div className="container-head-dash" style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
+                <div className="title-screen">
+                    Gestión de empleados
+                </div>
+                <div style={{ display: 'flex', justifyContent: 'flex-end', gap: '20px' }}>
+                    <Button className='add-button' type="primary" icon={<PlusOutlined />}>Nuevo Empleado</Button>
+                </div> 
+            </div>    
+            
+            <Layout style={{ minHeight: '65vh', overflow: 'hidden'}}>
+                <Sider width={250} className="sider">
+                    <Card title="Filtros" bordered={false} className="filters-card">
+                        <Button type="primary" className="clear-filters-button">Limpiar filtros</Button>
+                    </Card>
+                </Sider>
 
-            <Layout className="layout-content">
-                <Content style={{ padding: '24px' }}>
-                    <div style={{ marginBottom: '16px', display: 'flex', justifyContent: 'flex-end', gap: '20px' }}>
-                        <Button className='add-button' type="primary" icon={<PlusOutlined />}>Nuevo Empleado</Button>
-                    </div>
-                    <Table
-                        columns={columns}
-                        dataSource={data}
-                        rowKey="key"
-                        rowSelection={rowSelection}
-                        pagination={{
-                            current: currentPage,
-                            pageSize: pageSize,
-                            onChange: (page, size) => {
-                                setCurrentPage(page);
-                                setPageSize(size);
-                            },
-                            showSizeChanger: true,
-                            pageSizeOptions: ['5', '10', '20', '50'],
-                        }}
-                        onChange={handleTableChange}
-                    />
-                </Content>
+                <Layout className="layout-content">
+                    <Content style={{ padding: '0px 20px', marginTop: '-20px'}}>
+                        <Table
+                            columns={columns}
+                            dataSource={data}
+                            rowKey="key"
+                            rowSelection={rowSelection}
+                            pagination={{
+                                current: currentPage,
+                                pageSize: pageSize,
+                                onChange: (page, size) => {
+                                    setCurrentPage(page);
+                                    setPageSize(size);
+                                },
+                                showSizeChanger: true,
+                                pageSizeOptions: ['5', '10', '20', '50'],
+                            }}
+                            onChange={handleTableChange}
+                        />
+                    </Content>
+                </Layout>
             </Layout>
-        </Layout>
+        </div>
     );
 };
 
