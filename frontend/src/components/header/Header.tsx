d190b9694027eda1cc35d90a35ab0573920c0fc8
Anahi Peralta <anahiperalta2727@gmail.com>
2024-10-30 11:42:47 -0300

    añadi boton para cerrar sesión

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,6 +1,6 @@
 // src/components/Header.tsx
 import React from 'react';
-import { Menu, Layout } from 'antd';
+import { Menu, Layout, Button } from 'antd';
 import { Link } from 'react-router-dom';
 import './header.css';
 
@@ -9,10 +9,13 @@ const { Header } = Layout;
 const AppHeader: React.FC = () => {
   return (
     <Header style={{ background: '#f4f5f7', padding: 0, display: 'block' }}>
-      <div className="container-logo">
+      <div className="container-logo" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', background: '#f4f5f7'}}>
         <div className="logo">
           Brunnete
         </div>
+        <div style={{ display: 'flex', justifyContent: 'flex-end', paddingRight: '20px' }}>
+            <Button className='add-button' type="primary">Cerrar sesión</Button>
+        </div> 
       </div>
       <div className="horizontal-menu">
         <Menu className="menu-custom" theme="dark" mode="horizontal">
