5a7a310be7f985db16f5fa102a35077f59434a33
Juli <rivasaaron154@gmail.com>
2024-11-25 16:39:35 -0300

    nuevos modelos de datos

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,6 @@
 # models.py
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
 class Venta(models.Model):
     fecha = models.DateField()
@@ -11,7 +11,7 @@ class Venta(models.Model):
     pago = models.CharField(max_length=100, default="")
     facturacion = models.CharField(max_length=100, default="")
     nombre_venta = models.CharField(max_length=100, default="")
-
+    caja = models.ForeignKey('Caja', related_name='ventas', on_delete=models.CASCADE, null=True)
     def __str__(self):
         return f"Venta {self.id} - {self.fecha}"
 
@@ -41,6 +41,8 @@ class Caja(models.Model):
     fecha_hs_cierre_caja = models.DateTimeField(blank=True, null=True)
     monto_inicial_caja = models.DecimalField(max_digits=10, decimal_places=2)
     total_saldo_caja = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    usuario_apertura = models.ForeignKey(User, related_name='apertura_cajas', on_delete=models.SET_NULL, null=True, blank=True)
+    usuario_cierre = models.ForeignKey(User, related_name='cierre_cajas', on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         estado = "Abierta" if self.estado_caja else "Cerrada"
--- frontend/src/components/caja/caja.tsx
+++ frontend/src/components/caja/caja.tsx
@@ -1,16 +1,8 @@
-// import { Layout, Table, Button, Input, Space, Card } from 'antd';
-import { PlusCircleOutlined, DeleteOutlined, FolderOutlined, MenuOutlined } from '@ant-design/icons';
-import './caja.css'
-// import React, { useState } from 'react';
-import moment from 'moment';
-
-
-
 import React, { useEffect, useState } from 'react';
-import { Layout, Button, Card, Divider, Radio, Table, Flex, Breadcrumb } from 'antd';
-import type { TableColumnsType, TableProps } from 'antd';
-import { start } from 'repl';
-import { listarCajas } from '../../services/cajaService';
+import { Layout, Button, Table, Breadcrumb, Card } from 'antd';
+import { PlusCircleOutlined, MenuOutlined, DeleteOutlined } from '@ant-design/icons';
+import { listarCajas } from '../../services/cajaService'; // Asegúrate de importar el servicio correctamente
+import moment from 'moment';
 
 const { Sider, Content } = Layout; //destructuro el layout puede ser header sider content footer
 
@@ -23,125 +15,93 @@ interface DataType {
     montoFin: number;
 }
 
-
-const columns: TableColumnsType<DataType> = [
-    { title: 'Fecha', dataIndex: 'fecha', sorter: (a: any, b: any) => moment(a.fecha, 'DD/MM/YYYY').unix() - moment(b.fecha, 'DD/MM/YYYY').unix() },
-    {
-        title: 'Turno', dataIndex: 'turno',
-        filters: [
-            {
-                text: 'Tarde',
-                value: 'Tarde',
-            },
-            {
-                text: 'Mañana',
-                value: 'Mañana',
-            },
-        ],
-        filterMode: 'tree',
-        filterSearch: true,
-        onFilter: (value, record) => record.turno.startsWith(value as string),
-        width: '30%',
-    },
-    { title: 'Estado Caja', dataIndex: 'estado' },
-    { title: 'Monto Inicial', dataIndex: 'montoIni' },
-    { title: 'Monto Final', dataIndex: 'montoFin' },
-    {
-        title: 'Opciones', key: 'opciones', render: (_: any, record: any) => (
-            <span>
-                <Button icon={<DeleteOutlined style={{ marginRight: '30px' }} />} type="link" danger onClick={() => console.log('Eliminar', record)} />
-                <Button className='add-button' onClick={() => console.log('Editar', record)}>Detalle Venta</Button>
-
-            </span>
-        ),
-    },
+const columns = [
+  { title: 'Fecha', dataIndex: 'fecha', sorter: (a: any, b: any) => moment(a.fecha, 'DD/MM/YYYY').unix() - moment(b.fecha, 'DD/MM/YYYY').unix() },
+  { title: 'Turno', dataIndex: 'turno' },
+  { title: 'Estado Caja', dataIndex: 'estado' },
+  { title: 'Monto Inicial', dataIndex: 'montoIni' },
+  { title: 'Monto Final', dataIndex: 'montoFin' },
+  {
+    title: 'Opciones', key: 'opciones', render: (_: any, record: any) => (
+      <span>
+        <Button icon={<DeleteOutlined />} type="link" danger onClick={() => console.log('Eliminar', record)} />
+        <Button className='add-button' onClick={() => console.log('Editar', record)}>Detalle Venta</Button>
+      </span>
+    ),
+  },
 ];
-
-const data: DataType[] = [
-    { key: '1', fecha: moment().format('DD/MM/YYYY'), turno: "Mañana", estado: 'abierta', montoIni: 100000, montoFin: 500000 }, //ver como poner fecha
-    { key: '2', fecha: moment().subtract(1, 'month').format('DD/MM/YYYY'), turno: "Mañana", estado: 'abierta', montoIni: 100000, montoFin: 500000 },
-    { key: '3', fecha: moment().subtract(2, 'year').format('DD/MM/YYYY'), turno: "Mañana", estado: 'abierta', montoIni: 100000, montoFin: 500000 },
-    { key: '4', fecha: moment().subtract(2, 'days').format('DD/MM/YYYY'), turno: "Mañana", estado: 'abierta', montoIni: 100000, montoFin: 500000 },
-    { key: '5', fecha: moment().subtract(2, 'days').format('DD/MM/YYYY'), turno: "Tarde", estado: 'abierta', montoIni: 100000, montoFin: 500000 },
-    { key: '6', fecha: moment().subtract(2, 'days').format('DD/MM/YYYY'), turno: "Tarde", estado: 'abierta', montoIni: 100000, montoFin: 500000 },
-    { key: '7', fecha: moment().subtract(2, 'days').format('DD/MM/YYYY'), turno: "Tarde", estado: 'abierta', montoIni: 100000, montoFin: 500000 },
-    { key: '8', fecha: moment().subtract(2, 'days').format('DD/MM/YYYY'), turno: "Tarde", estado: 'abierta', montoIni: 100000, montoFin: 500000 },
-];
-
-// El objeto rowSelection indica la necesidad de selección de filas
-const rowSelection: TableProps<DataType>['rowSelection'] = {
-    onChange: (selectedRowKeys: React.Key[], selectedRows: DataType[]) => { //on change es una función que se ejecuta cada vez que cambia la selección de filas.
-        console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows); //Es un array de claves (React.Key[]) que representa las claves únicas de las filas seleccionadas.
-        // selectrows Es un array de objetos (DataType[]) que contiene los datos completos de las filas seleccionadas.
-    },
-    getCheckboxProps: (record: DataType) => ({ //record: Representa el objeto de datos de la fila actual.
-        disabled: record.turno === 'Disabled User', //indica si el checkbox esta deshabilitado.si el name del registro es 'Disabled User' lo deshabilita
-        name: record.turno,
-    }),
-};
-
-
 
 const CajaTemp: React.FC = () => {
-    const [selectionType, setSelectionType] = useState<'checkbox' | 'radio'>('checkbox'); //por deefecto checbok xq borre el radio
-    
-    useEffect(() => {
-        const fetchCajas = async () => {
-            try {
-                const data = await listarCajas();
-                console.log(data, "dataCaja")
-            } catch (error) {
-                console.error('Error al obtener las cajas:', error);
-            } finally {
-            }
-        };
-    
-        fetchCajas();
-    }, []);
+  const [cajas, setCajas] = useState<DataType[]>([]);  // Estado para almacenar las cajas
+  const [loading, setLoading] = useState(false);  // Estado para manejar la carga de datos
 
-    return (
-        <div style={{ marginTop: '4rem' }}>
-            <div className="container-bread-crumb" style={{ display: 'flex', alignItems: 'center', padding: '15px 0px' }}>
-                <MenuOutlined style={{ fontSize: '14px', marginRight: '8px' }} />
-                <Breadcrumb>
-                    <Breadcrumb.Item>Inicio</Breadcrumb.Item>
-                    <Breadcrumb.Item className="item-focus">Caja</Breadcrumb.Item>
-                </Breadcrumb>
-            </div>
-            <div className="container-head-dash" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-                <div className="title-screen">
-                    Caja
-                </div>
-                <div style={{ display: 'flex', justifyContent: 'flex-end', gap: '20px' }}>
-                    <Button className='add-button ' icon={<PlusCircleOutlined />}>Monto Inicial</Button>
-                    <Button className='add-button ' icon={<PlusCircleOutlined />}>Abrir Caja</Button>
-                </div>
-            </div>
+  // Llamada a la API para obtener las cajas
+  useEffect(() => {
+    const fetchCajas = async () => {
+      setLoading(true);
+      try {
+        const data = await listarCajas();  // Llamada al servicio
+        const formattedData = data.map((caja: any, index: number) => ({
+          key: index + 1,
+          fecha: moment(caja.fecha_hs_aper_caja).format('DD/MM/YYYY'),
+          turno: caja.estado_caja ? 'Abierta' : 'Cerrada',
+          estado: caja.estado_caja ? 'Abierta' : 'Cerrada',
+          montoIni: parseFloat(caja.monto_inicial_caja),
+          montoFin: caja.total_saldo_caja ? parseFloat(caja.total_saldo_caja) : 0,
+        }));
+        setCajas(formattedData);
+      } catch (error) {
+        console.error('Error al obtener las cajas:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-            <Layout style={{ minHeight: '65vh', overflow: 'hidden' }}>
-                <Sider width={250} className="sider">
-                    <Card title="Filtros" bordered={false} className="filters-card">
-                        <Button type="primary" className="clear-filters-button">Limpiar filtros</Button>
-                    </Card>
-                </Sider>
+    fetchCajas();
+  }, []);
 
-                <Layout className="layout-content">
-                    <Content style={{ padding: '0px 20px', marginTop: '-20px' }}>
-                        <Table<DataType>
-                            rowSelection={{ type: selectionType, ...rowSelection }}
-                            columns={columns}
-                            dataSource={data}
-                            pagination={{
-                                pageSize: 5,
-                                pageSizeOptions: ['5', '10', '20'],
-                                defaultCurrent: 1,
-                                position: ['bottomCenter'],
-                            }} />
-                    </Content>
-                </Layout>
-            </Layout>
+  return (
+    <div style={{ marginTop: '4rem' }}>
+      <div className="container-bread-crumb" style={{ display: 'flex', alignItems: 'center', padding: '15px 0px' }}>
+        <MenuOutlined style={{ fontSize: '14px', marginRight: '8px' }} />
+        <Breadcrumb>
+          <Breadcrumb.Item>Inicio</Breadcrumb.Item>
+          <Breadcrumb.Item>Caja</Breadcrumb.Item>
+        </Breadcrumb>
+      </div>
+      <div className="container-head-dash" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+        <div className="title-screen">Caja</div>
+        <div style={{ display: 'flex', justifyContent: 'flex-end', gap: '20px' }}>
+          <Button className='add-button ' icon={<PlusCircleOutlined />}>Abrir Caja</Button>
         </div>
-    );
+      </div>
+
+      <Layout style={{ minHeight: '65vh', overflow: 'hidden' }}>
+        <Sider width={250} className="sider">
+          <Card title="Filtros" bordered={false} className="filters-card">
+            <Button type="primary" className="clear-filters-button">Limpiar filtros</Button>
+          </Card>
+        </Sider>
+
+        <Layout className="layout-content">
+          <Content style={{ padding: '0px 20px', marginTop: '-20px' }}>
+            <Table<DataType>
+              columns={columns}
+              dataSource={cajas}  // Utiliza los datos obtenidos de la API
+              rowKey="key"
+              pagination={{
+                pageSize: 5,
+                pageSizeOptions: ['5', '10', '20'],
+                defaultCurrent: 1,
+                position: ['bottomCenter'],
+              }}
+              loading={loading}  // Agrega la propiedad loading para mostrar el spinner de carga
+            />
+          </Content>
+        </Layout>
+      </Layout>
+    </div>
+  );
 };
 
 export default CajaTemp;
